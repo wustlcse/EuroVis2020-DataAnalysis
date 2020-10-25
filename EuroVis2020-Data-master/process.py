294d7534007b2b3a580909ecab02098f9af19638
wustlcse <zhengliang@wustl.edu>
2020-10-24 19:19:12 -0700

    update again

--- EuroVis2020-Data-master/process.py
+++ EuroVis2020-Data-master/process.py
@@ -40,12 +40,24 @@ with open('data/article_info.csv', 'r') as file:
 
 for key, value in result.items():
     cur = result[key] # key --> Participant code; cur --> each participant's individual info dict
+
+    # stats collected and default values
     cur['total_article_duration'] = 0
+
     cur['article_stats'] = {}
+
+    cur['total_unique_article_read_count'] = 0
+
+    cur['relevant_unique_article_read_count'] = 0
+
+    cur['total_exploration_path_length'] = 0
+
+    cur['total_article_read_count_in_path'] = 0
+
+    cur['relevant_article_read_count_in_path'] = 0
+
     if len(cur['raw_data_list']) == 0:
-        cur['total_article_duration'] = 0
-        cur['total_exploration_path_length'] = 0
-        cur['avg_time_per_article'] = 'NA'
+        cur['avg_time_per_article'] = 'NA' # finish cur['avg_time_per_article']
     else:
         for r in cur['raw_data_list']:
             dur = r[duration_enum]
@@ -53,11 +65,22 @@ for key, value in result.items():
             if article in cur['article_stats']:
                 cur['article_stats'][article] += 1
             else:
-                cur['article_stats'][article] = 1
+                cur['article_stats'][article] = 1 # finish cur['article_stats']
+                if articles_info[article]['RelevantToTask'] == 'yes':
+                    cur['relevant_unique_article_read_count'] += 1 # finish cur['relevant_unique_article_read_count']
+
             if dur != 'NA':
-                cur['total_article_duration'] += int(dur)
-        cur['total_exploration_path_length'] = len(cur['raw_data_list'])
-        cur['avg_time_per_article'] = cur['total_article_duration']/len(cur['raw_data_list'])
+                cur['total_article_duration'] += int(dur) # finish cur['total_article_duration']
+
+            cur['total_article_read_count_in_path'] += 1 # finish cur['total_article_read_count_in_path']
+
+            if articles_info[article]['RelevantToTask'] == 'yes':
+                cur['relevant_article_read_count_in_path'] += 1  # finish cur['relevant_article_read_count_in_path']
+
+        cur['total_exploration_path_length'] = len(cur['raw_data_list']) # finish cur['total_exploration_path_length']
+        cur['avg_time_per_article'] = cur['total_article_duration']/len(cur['raw_data_list']) # finish cur['avg_time_per_article']
+        cur['total_unique_article_read_count'] = len(cur['article_stats']) # finish cur['total_unique_article_read_count']
+
 
 pprint.pprint(result)
 
