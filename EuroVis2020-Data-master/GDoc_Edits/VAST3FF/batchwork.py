d3c67d4e1814fa6aa57389726b7a19b5116584da
ZL Liu <zlliu@ZLs-MacBook-Pro.local>
2020-11-30 16:56:30 -0800

    remove gdocs

--- EuroVis2020-Data-master/GDoc_Edits/VAST3FF/batchwork.py
+++
@@ -1,8 +0,0 @@
-import os,pprint
-files = os.listdir(".")
-filtered = [k for k in files if 'VAST' in k]
-sorted_files = sorted(filtered, key=lambda item: int(item.split('_')[2]))
-pprint.pprint(sorted_files)
-for i in range(len(sorted_files)):
-	if i != 0:
-		os.system('patch ' + sorted_files[0] + ' ' + sorted_files[i])
